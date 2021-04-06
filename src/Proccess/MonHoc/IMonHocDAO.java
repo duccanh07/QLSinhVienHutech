eae67175ac05c85ebd3e8f35c72067ba3206f46c
HoangTuan07 <tuannguyen.150999@gmail.com>
2021-04-06 23:24:59 +0700

    design proccess MonHoc

---
+++ src/Proccess/MonHoc/IMonHocDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proccess.MonHoc;
+
+/**
+ *
+ * @author 84378
+ */
+public class IMonHocDAO {
+    
+}
